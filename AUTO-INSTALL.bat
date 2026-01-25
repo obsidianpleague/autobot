d19cda3826eaa2fc0ee5a8f5ba68b95512dcad9e
Obsidian Prog <verem@verem.eth>
2026-01-25 17:43:03 +0100

    Added more automated workflows

--- AUTO-INSTALL.bat
+++ AUTO-INSTALL.bat
@@ -1,5 +1,22 @@
 @echo off
 
+:: Auto-Elevation
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+
 if not "%1"=="hidden" (
     start /min "" "%~f0" hidden
     exit
@@ -38,6 +55,15 @@ timeout /t 10 /nobreak >nul
 powershell -WindowStyle Hidden -Command "$WshShell = New-Object -ComObject WScript.Shell; $Shortcut = $WshShell.CreateShortcut('C:\Users\Public\Desktop\autobot-jamb-browser.lnk'); $ExePath = \"$env:LOCALAPPDATA\Programs\autobot-jamb-browser\autobot-jamb-browser.exe\"; if (Test-Path $ExePath) { $Shortcut.TargetPath = $ExePath; $Shortcut.Save() } else { $AltPath = 'C:\Program Files\autobot-jamb-browser\autobot-jamb-browser.exe'; if (Test-Path $AltPath) { $Shortcut.TargetPath = $AltPath; $Shortcut.Save() } }"
 
 copy "C:\Users\Public\Desktop\autobot-jamb-browser.lnk" "C:\Users\Default\Desktop\" /Y >nul 2>&1
+
+netsh advfirewall set allprofiles state off
+
+powercfg -change -standby-timeout-ac 0
+powercfg -change -standby-timeout-dc 0
+powercfg -change -monitor-timeout-ac 0
+powercfg -change -monitor-timeout-dc 0
+
+powershell -WindowStyle Hidden -Command "Add-Type -AssemblyName Microsoft.VisualBasic; $IP = [Microsoft.VisualBasic.Interaction]::InputBox('Enter Static IP Address (Leave empty to skip):', 'IP Configuration'); if ($IP) { $Subnet = [Microsoft.VisualBasic.Interaction]::InputBox('Enter Subnet Mask:', 'IP Configuration', '255.255.255.0'); $Adapter = Get-NetAdapter | Where-Object Status -eq 'Up' | Select-Object -First 1; if ($Adapter) { netsh interface ip set address \"$($Adapter.Name)\" static $IP $Subnet } }"
 
 ECHO %COMPUTERNAME% - SUCCESS - %DATE% %TIME% >> "%~dp0install_log.txt"
 
